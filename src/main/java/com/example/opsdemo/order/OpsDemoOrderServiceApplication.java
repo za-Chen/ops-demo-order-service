a4fec8f5d6d3d405b2fb82e83373e848a9aff274
cza <2385025840@qq.com>
2026-08-03 14:01:12 +0800

    Initial project

---
+++ src/main/java/com/example/opsdemo/order/OpsDemoOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.opsdemo.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsDemoOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpsDemoOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/opsdemo/order/OpsDemoOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opsdemo.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsDemoOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
